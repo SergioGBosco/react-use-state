16da6179e5b3a64d7f2657e0ec5699693387648d
SergioGBosco <sergiogibosco@gmail.com>
2025-08-28 17:09:47 +0200

    feat: add card for show description of lenguage

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,17 @@
-
+import CardLenguage from "./CardLenguage"
+import NavBar from "./NavBar"
 const Main = () => {
   return (
     <main>
       <div className="container">
         <div className="row">
-          <div className="col-100">
-            <div className="button">HTML
-            </div>
-            <div className="col-100">
-              <div className="card-lenguage">
-                <h4>HTML</h4>
-                <p>"HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini."</p>
-              </div>
-            </div>
-          </div>
+          <NavBar />
+        </div>
+        <div className="row">
+          <CardLenguage />
         </div>
       </div>
-    </main>
+    </main >
   )
 }
 
